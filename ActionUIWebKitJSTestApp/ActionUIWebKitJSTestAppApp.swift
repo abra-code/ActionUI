f2b0f16166ba278f228c1e37b77e94bb316cfdd6
Tom <tom-dev@abracode.com>
2025-10-15 01:48:25 -0700

    Start ActionUIWebKitJSTestApp
    Empty initally

---
+++ ActionUIWebKitJSTestApp/ActionUIWebKitJSTestAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ActionUIWebKitJSTestAppApp.swift
+//  ActionUIWebKitJSTestApp
+//
+//  Created by Tomasz Kukielka on 10/15/25.
+//
+
+import SwiftUI
+
+@main
+struct ActionUIWebKitJSTestAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
